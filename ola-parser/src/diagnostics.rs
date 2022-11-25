e14f2dfbb48af859aac94df05fab7d51c08ce218
hyl <563807243@qq.com>
2022-11-25 10:34:05 +0800

    add lalrpop grammar

---
+++ ola-parser/src/diagnostics.rs
@@ -0,0 +1,200 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::pt;
+use crate::pt::Loc;
+
+#[derive(Clone, Debug, Hash, PartialOrd, Ord, PartialEq, Eq)]
+pub enum Level {
+    Debug,
+    Info,
+    Warning,
+    Error,
+}
+
+impl Level {
+    pub fn to_string(&self) -> &'static str {
+        match self {
+            Level::Debug => "debug",
+            Level::Info => "info",
+            Level::Warning => "warning",
+            Level::Error => "error",
+        }
+    }
+}
+
+#[derive(Clone, Debug, Hash, PartialOrd, Ord, PartialEq, Eq)]
+pub enum ErrorType {
+    None,
+    ParserError,
+    SyntaxError,
+    DeclarationError,
+    CastError,
+    TypeError,
+    Warning,
+}
+
+#[derive(Clone, Debug, Hash, PartialOrd, Ord, PartialEq, Eq)]
+pub struct Note {
+    pub loc: pt::Loc,
+    pub message: String,
+}
+
+#[derive(Clone, Debug, Hash, PartialOrd, Ord, PartialEq, Eq)]
+pub struct Diagnostic {
+    pub loc: pt::Loc,
+    pub level: Level,
+    pub ty: ErrorType,
+    pub message: String,
+    pub notes: Vec<Note>,
+}
+
+impl Diagnostic {
+    pub fn debug(loc: Loc, message: String) -> Self {
+        Diagnostic {
+            level: Level::Debug,
+            ty: ErrorType::None,
+            loc,
+            message,
+            notes: Vec::new(),
+        }
+    }
+
+    pub fn info(loc: Loc, message: String) -> Self {
+        Diagnostic {
+            level: Level::Info,
+            ty: ErrorType::None,
+            loc,
+            message,
+            notes: Vec::new(),
+        }
+    }
+
+    pub fn parser_error(loc: Loc, message: String) -> Self {
+        Diagnostic {
+            level: Level::Error,
+            ty: ErrorType::ParserError,
+            loc,
+            message,
+            notes: Vec::new(),
+        }
+    }
+
+    pub fn error(loc: Loc, message: String) -> Self {
+        Diagnostic {
+            level: Level::Error,
+            ty: ErrorType::SyntaxError,
+            loc,
+            message,
+            notes: Vec::new(),
+        }
+    }
+
+    pub fn decl_error(loc: Loc, message: String) -> Self {
+        Diagnostic {
+            level: Level::Error,
+            ty: ErrorType::DeclarationError,
+            loc,
+            message,
+            notes: Vec::new(),
+        }
+    }
+
+    pub fn cast_error(loc: Loc, message: String) -> Self {
+        Diagnostic {
+            level: Level::Error,
+            ty: ErrorType::CastError,
+            loc,
+            message,
+            notes: Vec::new(),
+        }
+    }
+
+    pub fn cast_error_with_note(loc: Loc, message: String, note_loc: Loc, note: String) -> Self {
+        Diagnostic {
+            level: Level::Error,
+            ty: ErrorType::CastError,
+            loc,
+            message,
+            notes: vec![Note {
+                loc: note_loc,
+                message: note,
+            }],
+        }
+    }
+
+    pub fn type_error(loc: Loc, message: String) -> Self {
+        Diagnostic {
+            level: Level::Error,
+            ty: ErrorType::TypeError,
+            loc,
+            message,
+            notes: Vec::new(),
+        }
+    }
+
+    pub fn cast_warning(loc: Loc, message: String) -> Self {
+        Diagnostic {
+            level: Level::Warning,
+            ty: ErrorType::CastError,
+            loc,
+            message,
+            notes: Vec::new(),
+        }
+    }
+
+    pub fn warning(loc: Loc, message: String) -> Self {
+        Diagnostic {
+            level: Level::Warning,
+            ty: ErrorType::Warning,
+            loc,
+            message,
+            notes: Vec::new(),
+        }
+    }
+
+    pub fn warning_with_note(loc: Loc, message: String, note_loc: Loc, note: String) -> Self {
+        Diagnostic {
+            level: Level::Warning,
+            ty: ErrorType::Warning,
+            loc,
+            message,
+            notes: vec![Note {
+                loc: note_loc,
+                message: note,
+            }],
+        }
+    }
+
+    pub fn warning_with_notes(loc: Loc, message: String, notes: Vec<Note>) -> Self {
+        Diagnostic {
+            level: Level::Warning,
+            ty: ErrorType::Warning,
+            loc,
+            message,
+            notes,
+        }
+    }
+
+    pub fn error_with_note(loc: Loc, message: String, note_loc: Loc, note: String) -> Self {
+        Diagnostic {
+            level: Level::Error,
+            ty: ErrorType::None,
+            loc,
+            message,
+            notes: vec![Note {
+                loc: note_loc,
+                message: note,
+            }],
+        }
+    }
+
+    pub fn error_with_notes(loc: Loc, message: String, notes: Vec<Note>) -> Self {
+        Diagnostic {
+            level: Level::Error,
+            ty: ErrorType::None,
+            loc,
+            message,
+            notes,
+        }
+    }
+}
---
+++ ola-parser/src/lib.rs
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: Apache-2.0
+
+//! Solidity file parser
+use crate::lexer::LexicalError;
+use crate::lexer::Token;
+use crate::pt::CodeLocation;
+use crate::pt::Loc;
+use diagnostics::Diagnostic;
+use lalrpop_util::ParseError;
+
+pub mod diagnostics;
+pub mod doccomment;
+pub mod lexer;
+pub mod pt;
+#[cfg(test)]
+mod test;
+
+#[allow(clippy::all)]
+mod solidity {
+    include!(concat!(env!("OUT_DIR"), "/solidity.rs"));
+}
+
+/// Parse solidity file
+pub fn parse(
+    src: &str,
+    file_no: usize,
+) -> Result<(pt::SourceUnitPart, Vec<pt::Comment>), Vec<Diagnostic>> {
+    // parse phase
+    let mut comments = Vec::new();
+    let mut lexer_errors = Vec::new();
+    let mut lex = lexer::Lexer::new(src, file_no, &mut comments, &mut lexer_errors);
+
+    let parser_errors = &mut Vec::new();
+    let errors = &mut Vec::new();
+
+    let s = solidity::SourceUnitParser::new().parse(src, file_no, parser_errors, &mut lex);
+
+    for lexical_error in lex.errors {
+        errors.push(Diagnostic::parser_error(
+            lexical_error.loc(),
+            lexical_error.to_string(),
+        ))
+    }
+
+    for e in parser_errors {
+        errors.push(parser_error(&e.error, file_no));
+    }
+
+    if let Err(e) = s {
+        errors.push(parser_error(&e, file_no));
+        return Err(errors.to_vec());
+    }
+
+    if !errors.is_empty() {
+        Err(errors.to_vec())
+    } else {
+        Ok((s.unwrap(), comments))
+    }
+}
+
+fn parser_error(error: &ParseError<usize, Token, LexicalError>, file_no: usize) -> Diagnostic {
+    match &error {
+        ParseError::InvalidToken { location } => Diagnostic::parser_error(
+            Loc::File(file_no, *location, *location),
+            "invalid token".to_string(),
+        ),
+        ParseError::UnrecognizedToken {
+            token: (l, token, r),
+            expected,
+        } => Diagnostic::parser_error(
+            Loc::File(file_no, *l, *r),
+            format!(
+                "unrecognised token '{}', expected {}",
+                token,
+                expected.join(", ")
+            ),
+        ),
+        ParseError::User { error } => Diagnostic::parser_error(error.loc(), error.to_string()),
+        ParseError::ExtraToken { token } => Diagnostic::parser_error(
+            Loc::File(file_no, token.0, token.2),
+            format!("extra token '{}' encountered", token.0),
+        ),
+        ParseError::UnrecognizedEOF { expected, location } => Diagnostic::parser_error(
+            Loc::File(file_no, *location, *location),
+            format!("unexpected end of file, expecting {}", expected.join(", ")),
+        ),
+    }
+}
