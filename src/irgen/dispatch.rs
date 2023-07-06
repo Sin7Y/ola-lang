d06749d8b6b1983bb9f63765715654166fcc583f
yp945 <pan107104@outlook.com>
2023-07-06 12:26:18 +0800

    refactor struct elem alignment.

--- src/irgen/dispatch.rs
+++ src/irgen/dispatch.rs
@@ -1,169 +1,161 @@
-use inkwell::{
-    basic_block::BasicBlock,
-    values::{BasicMetadataValueEnum, FunctionValue, IntValue, PointerValue},
-    AddressSpace,
-};
-use num_bigint::{BigInt, Sign};
-use num_traits::ToPrimitive;
+// use inkwell::{
+//     basic_block::BasicBlock,
+//     values::{BasicMetadataValueEnum, BasicValueEnum, FunctionValue, IntValue,
+// PointerValue},     AddressSpace,
+// };
+// use num_bigint::{BigInt, Sign};
+// use num_traits::ToPrimitive;
 
-use crate::sema::ast::{Expression, Namespace, Type};
+// use crate::sema::ast::{Expression, Namespace, Type};
 
-use super::binary::Binary;
+// use super::{binary::Binary, encoding::abi_decode};
 
-fn public_function_prelude<'a>(
-    binary: &Binary<'a>,
-    function: FunctionValue,
-) -> (IntValue<'a>, IntValue<'a>, PointerValue<'a>) {
-    let entry = binary.context.append_basic_block(function, "entry");
+// fn public_function_prelude<'a>(
+//     binary: &Binary<'a>,
+//     function: FunctionValue,
+// ) -> (IntValue<'a>, IntValue<'a>, PointerValue<'a>) {
+//     let entry = binary.context.append_basic_block(function, "entry");
 
-    binary.builder.position_at_end(entry);
+//     binary.builder.position_at_end(entry);
 
-    let input = binary
-        .builder
-        .build_call(
-            binary.module.get_function("contract_input").unwrap(),
-            &[],
-            "",
-        )
-        .try_as_basic_value()
-        .left()
-        .unwrap();
+//     let input = binary
+//         .builder
+//         .build_call(
+//             binary.module.get_function("contract_input").unwrap(),
+//             &[],
+//             "",
+//         )
+//         .try_as_basic_value()
+//         .left()
+//         .unwrap();
 
-    binary.contract_input(input)
-}
+//     binary.contract_input(input)
+// }
 
-/// Emits the "deploy" function if `init` is `Some`, otherwise emits the "call"
-/// function.
-pub fn gen_contract_entrance(init: Option<FunctionValue>, bin: &mut Binary, ns: &Namespace) {
-    let ty = bin.context.void_type().fn_type(&[], false);
-    let name = if init.is_some() { "deploy" } else { "call" };
-    let func = bin.module.add_function(name, ty, None);
-    let (selector, input_length, input) = public_function_prelude(bin, func);
-    if let Some(initializer) = init {
-        bin.builder.build_call(initializer, &[], "");
-    }
-    let function_dispatch = bin.module.get_function("function_dispatch").unwrap();
-    let args = vec![
-        BasicMetadataValueEnum::IntValue(selector),
-        BasicMetadataValueEnum::IntValue(input_length),
-        BasicMetadataValueEnum::PointerValue(input),
-    ];
-    bin.builder
-        .build_call(function_dispatch, &args, "function_dispatch");
-    bin.builder.build_unreachable();
-}
+// /// Emits the "deploy" function if `init` is `Some`, otherwise emits the
+// "call" /// function.
+// pub fn gen_contract_entrance(init: Option<FunctionValue>, bin: &mut Binary,
+// ns: &Namespace) {     let ty = bin.context.void_type().fn_type(&[], false);
+//     let name = if init.is_some() { "deploy" } else { "call" };
+//     let func = bin.module.add_function(name, ty, None);
+//     let (selector, input_length, input) = public_function_prelude(bin, func);
+//     if let Some(initializer) = init {
+//         bin.builder.build_call(initializer, &[], "");
+//     }
+//     let function_dispatch =
+// bin.module.get_function("function_dispatch").unwrap();     let args = vec![
+//         BasicMetadataValueEnum::IntValue(selector),
+//         BasicMetadataValueEnum::IntValue(input_length),
+//         BasicMetadataValueEnum::PointerValue(input),
+//     ];
+//     bin.builder
+//         .build_call(function_dispatch, &args, "function_dispatch");
+//     bin.builder.build_unreachable();
+// }
 
-/// Emits the "function_dispatch" function.
-/// This function is called by the "deploy" and "call" functions.
-/// It dispatches the call to the appropriate function.
-/// It is called with the following arguments:
-/// - selector: the first 4 bytes of the input data
-/// - input_length: the length of the input data
-/// - input: a pointer to the input data
-/// It returns the following values:
+// /// Emits the "function_dispatch" function.
+// /// This function is called by the "deploy" and "call" functions.
+// /// It dispatches the call to the appropriate function.
+// /// It is called with the following arguments:
+// /// - selector: the first 4 bytes of the input data
+// /// - input_length: the length of the input data
+// /// - input: a pointer to the input data
+// /// It returns the following values:
 
-pub fn gen_func_dispatch(bin: &mut Binary, ns: &Namespace) {
-    let ty = bin.context.void_type().fn_type(
-        &[
-            bin.context.i64_type().into(),
-            bin.context.i64_type().into(),
-            bin.context
-                .i64_type()
-                .ptr_type(AddressSpace::default())
-                .into(),
-        ],
-        false,
-    );
-    let func_value = bin.module.add_function("function_dispatch", ty, None);
-    let entry = bin.context.append_basic_block(func_value, "entry");
+// pub fn gen_func_dispatch(bin: &mut Binary, ns: &Namespace) {
+//     let ty = bin.context.void_type().fn_type(
+//         &[
+//             bin.context.i64_type().into(),
+//             bin.context.i64_type().into(),
+//             bin.context
+//                 .i64_type()
+//                 .ptr_type(AddressSpace::default())
+//                 .into(),
+//         ],
+//         false,
+//     );
+//     let func_value = bin.module.add_function("function_dispatch", ty, None);
+//     let entry = bin.context.append_basic_block(func_value, "entry");
 
-    let selector = func_value.get_nth_param(0).unwrap().into_int_value();
+//     let selector = func_value.get_nth_param(0).unwrap().into_int_value();
+//     let input_length = func_value.get_nth_param(1).unwrap().into_int_value();
+//     let input = func_value.get_nth_param(2).unwrap().into_pointer_value();
 
-    bin.builder.position_at_end(entry);
+//     bin.builder.position_at_end(entry);
 
-    let default_case = bin
-        .context
-        .append_basic_block(func_value, "missing_function");
+//     let default_case = bin
+//         .context
+//         .append_basic_block(func_value, "missing_function");
 
-    let selector_cases = ns
-        .functions
-        .iter()
-        .enumerate()
-        .filter_map(|(func_no, func)| {
-            let selector = BigInt::from_bytes_le(Sign::Plus, &func.selector());
-            let case = bin
-                .context
-                .i64_type()
-                .const_int(selector.to_u64().unwrap(), false);
-            Some((case, dispatch_case(func_no, bin, func_value, ns)))
-        })
-        .collect::<Vec<(IntValue, inkwell::basic_block::BasicBlock)>>();
-    bin.builder.position_at_end(entry);
-    bin.builder
-        .build_switch(selector, default_case, &selector_cases.as_ref());
+//     let selector_cases = ns
+//         .functions
+//         .iter()
+//         .enumerate()
+//         .filter_map(|(func_no, func)| {
+//             let selector = BigInt::from_bytes_le(Sign::Plus,
+// &func.selector());             let case = bin
+//                 .context
+//                 .i64_type()
+//                 .const_int(selector.to_u64().unwrap(), false);
+//             Some((
+//                 case,
+//                 dispatch_case(input_length, input, func_no, bin, func_value,
+// ns),             ))
+//         })
+//         .collect::<Vec<(IntValue, inkwell::basic_block::BasicBlock)>>();
+//     bin.builder.position_at_end(entry);
+//     bin.builder
+//         .build_switch(selector, default_case, &selector_cases.as_ref());
 
-    bin.builder.position_at_end(default_case);
-    bin.builder.build_unreachable();
+//     bin.builder.position_at_end(default_case);
+//     bin.builder.build_unreachable();
 
-    // build switch statement
-}
+//     // build switch statement
+// }
 
-/// Insert the dispatch logic for `func_no`. `func_no` may be a function or
-/// constructor. Returns the basic block number in which the dispatch logic was
-/// inserted.
-fn dispatch_case<'a>(
-    func_no: usize,
-    bin: &mut Binary<'a>,
-    func_value: FunctionValue<'a>,
-    ns: &Namespace,
-) -> BasicBlock<'a> {
-    let case_bb = bin
-        .context
-        .append_basic_block(func_value, &format!("func_{func_no}_dispatch"));
-    bin.builder.position_at_end(case_bb);
+// /// Insert the dispatch logic for `func_no`. `func_no` may be a function or
+// /// constructor. Returns the basic block number in which the dispatch logic
+// was /// inserted.
+// fn dispatch_case<'a>(
+//     input_length: IntValue<'a>,
+//     input: PointerValue<'a>,
+//     func_no: usize,
+//     bin: &mut Binary<'a>,
+//     func_value: FunctionValue<'a>,
+//     ns: &Namespace,
+// ) -> BasicBlock<'a> {
+//     let case_bb = bin
+//         .context
+//         .append_basic_block(func_value, &format!("func_{func_no}_dispatch"));
+//     bin.builder.position_at_end(case_bb);
+//     let mut args = vec![];
+//     // Decode input data if necessary
+//     let func = &ns.functions[func_no];
+//     if !func.params.is_empty() {
+//         args = abi_decode(
+//             bin,
+//             input_length,
+//             input,
+//             &func.params.iter().map(|p| p.ty.clone()).collect::<Vec<_>>(),
+//             func,
+//             func_value,
+//             ns,
+//         );
+//     }
 
-    // Decode input data if necessary
-    let func = &ns.functions[func_no];
-    if !func.params.is_empty() {
-        // let buf_len = Expression::Variable {
-        //     loc: Codegen,
-        //     ty: Uint(32),
-        //     var_no: self.input_len,
-        // };
-        // let arg_len = Expression::Subtract {
-        //     loc: Codegen,
-        //     ty: Uint(32),
-        //     overflowing: false,
-        //     left: buf_len.into(),
-        //     right: self.selector_len.clone(),
-        // };
-        // args = abi_decode(
-        //     &Codegen,
-        //     &self.input_ptr,
-        //     &cfg.params.iter().map(|p| p.ty.clone()).collect::<Vec<_>>(),
-        //     self.ns,
-        //     &mut self.vartab,
-        //     &mut self.cfg,
-        //     Some(Expression::Trunc {
-        //         loc: Codegen,
-        //         ty: Uint(32),
-        //         expr: arg_len.into(),
-        //     }),
-        // );
-    }
+//     let mut returns: Vec<usize> = Vec::with_capacity(func.returns.len());
+//     let mut return_tys: Vec<Type> = Vec::with_capacity(func.returns.len());
+//     let mut returns_expr: Vec<Expression> =
+// Vec::with_capacity(func.returns.len());
 
-    let mut returns: Vec<usize> = Vec::with_capacity(func.returns.len());
-    let mut return_tys: Vec<Type> = Vec::with_capacity(func.returns.len());
-    let mut returns_expr: Vec<Expression> = Vec::with_capacity(func.returns.len());
+//     // build call function
+//     let callee = &ns.functions[func_no];
+//     let callee_value = bin.module.get_function(&callee.name).unwrap();
+//     let ret_value = bin
+//         .builder
+//         .build_call(callee_value, &args, "")
+//         .try_as_basic_value()
+//         .left();
 
-    // build call function
-    let callee = &ns.functions[func_no];
-    let callee_value = bin.module.get_function(&callee.name).unwrap();
-    let ret_value = bin
-        .builder
-        .build_call(callee_value, &[], "")
-        .try_as_basic_value()
-        .left();
-
-    case_bb
-}
+//     case_bb
+// }
---
+++ src/irgen/encoding/buffer_validator.rs
@@ -0,0 +1,222 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::{
+    irgen::binary::Binary,
+    sema::ast::{Namespace, Type},
+};
+use inkwell::{
+    values::{FunctionValue, IntValue},
+    IntPredicate,
+};
+use num_bigint::BigInt;
+use num_traits::{ToPrimitive, Zero};
+use std::ops::AddAssign;
+
+/// When we are decoding serialized data from a bytes array, we must constantly
+/// verify if we are not reading past its ending. This struct helps us decrease
+/// the number of checks we do, by merging checks when we can determine the size
+/// of what to read beforehand.
+pub(crate) struct BufferValidator<'a> {
+    /// Saves the codegen::Expression that contains the buffer length.
+    buffer_length: IntValue<'a>,
+    /// The types we are supposed to decode
+    types: &'a [Type],
+    /// The argument whose size has already been accounted for when verifying
+    /// the buffer
+    verified_until: Option<usize>,
+    /// The argument we are analysing presently
+    current_arg: usize,
+}
+
+impl<'a> BufferValidator<'a> {
+    pub fn new(buffer_size_var: IntValue<'a>, types: &'a [Type]) -> BufferValidator<'a> {
+        BufferValidator {
+            buffer_length: buffer_size_var,
+            types,
+            verified_until: None,
+            current_arg: 0,
+        }
+    }
+
+    /// Set which item we are currently reading from the buffer
+    pub(super) fn set_argument_number(&mut self, arg_no: usize) {
+        self.current_arg = arg_no;
+    }
+
+    /// Initialize the validator, by verifying every type that has a fixed size
+    pub(super) fn initialize_validation(
+        &mut self,
+        bin: &Binary<'a>,
+        offset: IntValue<'a>,
+        func_value: FunctionValue<'a>,
+        ns: &Namespace,
+    ) {
+        // -1 means nothing has been verified yet
+        self.verified_until = None;
+        self._verify_buffer(bin, offset, func_value, ns);
+    }
+
+    /// Validate the buffer for the current argument, if necessary.
+    pub(super) fn validate_buffer(
+        &mut self,
+        bin: &Binary<'a>,
+        offset: IntValue<'a>,
+        func_value: FunctionValue<'a>,
+        ns: &Namespace,
+    ) {
+        // We may have already verified this
+        if self.verified_until.is_some() && self.current_arg <= self.verified_until.unwrap() {
+            return;
+        }
+
+        self._verify_buffer(bin, offset, func_value, ns);
+    }
+
+    /// Validate if a given offset is within the buffer's bound.
+    pub(super) fn validate_offset(
+        &self,
+        bin: &Binary<'a>,
+        offset: IntValue<'a>,
+        func_value: FunctionValue<'a>,
+        ns: &Namespace,
+    ) {
+        self.build_out_of_bounds_fail_branch(bin, offset, func_value, ns);
+    }
+
+    /// Checks if a buffer validation is necessary
+    pub(super) fn validation_necessary(&self) -> bool {
+        self.verified_until.is_none() || self.current_arg > self.verified_until.unwrap()
+    }
+
+    /// After an array validation, we do not need to re-check its elements.
+    pub(super) fn validate_array(&mut self) {
+        self.verified_until = Some(self.current_arg);
+    }
+
+    /// Validate if offset + size is within the buffer's boundaries
+    pub(super) fn validate_offset_plus_size(
+        &mut self,
+        bin: &Binary<'a>,
+        offset: IntValue<'a>,
+        size: IntValue<'a>,
+        func_value: FunctionValue<'a>,
+        ns: &Namespace,
+    ) {
+        if self.validation_necessary() {
+            let offset_to_validate = bin.builder.build_int_add(offset, size, "");
+
+            self.validate_offset(bin, offset_to_validate, func_value, ns);
+        }
+    }
+
+    /// Validates if we have read all the bytes in a buffer
+    pub(super) fn validate_all_bytes_read(
+        &self,
+        bin: &Binary<'a>,
+        end_offset: IntValue<'a>,
+        func_value: FunctionValue<'a>,
+        ns: &Namespace,
+    ) {
+        let cond = bin
+            .builder
+            .build_int_compare(IntPredicate::ULT, end_offset, self.buffer_length, "")
+            .into();
+
+        let invaild = bin
+            .context
+            .append_basic_block(func_value, "not_all_bytes_read");
+        let valid = bin.context.append_basic_block(func_value, "buffer_read");
+
+        bin.builder.build_conditional_branch(cond, invaild, valid);
+
+        bin.builder.position_at_end(invaild);
+        // TODO: This needs a proper error message
+
+        bin.builder.position_at_end(valid);
+    }
+
+    /// Auxiliary function to verify if the offset is valid.
+    fn _verify_buffer(
+        &mut self,
+        bin: &Binary<'a>,
+        offset: IntValue<'a>,
+        func_value: FunctionValue<'a>,
+        ns: &Namespace,
+    ) {
+        // Calculate the what arguments we can validate
+        let mut maximum_verifiable = self.current_arg;
+        for i in self.current_arg..self.types.len() {
+            if !self.types[i].is_dynamic(ns) {
+                maximum_verifiable = i;
+            } else {
+                break;
+            }
+        }
+
+        // It is not possible to validate anything
+        if maximum_verifiable == self.current_arg {
+            return;
+        }
+
+        // Create validation check
+        let mut advance = BigInt::zero();
+        for i in self.current_arg..=maximum_verifiable {
+            advance.add_assign(self.types[i].memory_size_of(ns));
+        }
+
+        let reach = bin.builder.build_int_add(
+            offset,
+            bin.context
+                .i64_type()
+                .const_int(advance.to_u64().unwrap(), false),
+            "",
+        );
+
+        self.verified_until = Some(maximum_verifiable);
+        self.build_out_of_bounds_fail_branch(bin, reach, func_value, ns);
+    }
+
+    /// Builds a branch for failing if we are out of bounds
+    fn build_out_of_bounds_fail_branch(
+        &self,
+        bin: &Binary<'a>,
+        offset: IntValue<'a>,
+        func_value: FunctionValue<'a>,
+        ns: &Namespace,
+    ) {
+        let cond = bin
+            .builder
+            .build_int_compare(IntPredicate::ULE, offset, self.buffer_length, "")
+            .into();
+
+        let inbounds_block = bin.context.append_basic_block(func_value, "inbounds");
+        let out_of_bounds_block = bin.context.append_basic_block(func_value, "out_of_bounds");
+        bin.builder
+            .build_conditional_branch(cond, inbounds_block, out_of_bounds_block);
+        bin.builder.position_at_end(out_of_bounds_block);
+        // TODO: Add an error message here
+
+        bin.builder.position_at_end(inbounds_block);
+    }
+
+    /// Create a new buffer validator to validate struct fields.
+    pub(super) fn create_sub_validator(&self, types: &'a [Type]) -> BufferValidator<'a> {
+        // If the struct has been previously validated, there is no need to validate it
+        // again, so verified_until and current_arg are set to type.len() to
+        // avoid any further validation.
+        BufferValidator {
+            buffer_length: self.buffer_length.clone(),
+            types,
+            verified_until: if self.validation_necessary() {
+                None
+            } else {
+                Some(types.len())
+            },
+            current_arg: if self.validation_necessary() {
+                0
+            } else {
+                types.len()
+            },
+        }
+    }
+}
